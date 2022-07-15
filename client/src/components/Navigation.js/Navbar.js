998f892f01a9bf9ab5b33f26044a5817e5eee537
Arundhati <arundhatiraicar12@gmail.com>
2022-07-16 01:28:37 +0530

    Navbar styling done

--- client/src/components/Navigation.js/Navbar.js
+++ client/src/components/Navigation.js/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link ,NavLink} from "react-router-dom";
-
+import "./Navbar.css"
 function Navbar({ user, setUser }) {
   function handleLogoutClick() {
     fetch("/logout", { method: "DELETE" }).then((r) => {
@@ -11,24 +11,13 @@ function Navbar({ user, setUser }) {
   }
 
   return (
-   <>
-     <h1>Navbar</h1>
-       
-        <NavLink   to="/">
-         Home
-        </NavLink>
-        
-        <NavLink  to="/airlines">
-          Airlines
-        </NavLink>
-        <NavLink   to="/myprofile">
-         My Profile
-        </NavLink>
-       
-        <button onClick={handleLogoutClick}>Logout</button>
-        
-      
-    </>
+    <nav class="nav navbarstyle">
+  <a class="nav-link  nlink"  href="/"> Home</a>
+  <a class="nav-link nlink" href="/airlines">  Airlines</a>
+  <a class="nav-link nlink" href="/myprofile"> My Profile</a>
+  <button type="button" class="btn btn-primary logoutbtn"onClick={handleLogoutClick}>Logout</button>
+</nav>
+
   );
 }
 
