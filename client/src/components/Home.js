133ab1d32f3f4a808de453e606c21779ac848088
Arundhati <arundhatiraicar12@gmail.com>
2022-07-16 02:13:39 +0530

    Project before experimenting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,57 @@
 import { useReducer } from "react";
-
+import { useState } from "react";
+import "./Styling.css"
+import Carousel from 'react-bootstrap/Carousel';
 function Home({user}){
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
     return (<>
-    <h1>Welcome {user.name}</h1>
+    <h1 class="greeting">Hey {user.name}! Welcome to JetSet</h1>
+
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+   
     </>)
 }
 export default Home;
